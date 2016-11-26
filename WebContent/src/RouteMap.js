a9e2a3b4c955962b70117a4cae10e22e77c2727a
jimmy huey <wuey99@gmail.com>
2016-11-25 17:22:59 -0800

    tidying

--- WebContent/src/RouteMap.js
+++ WebContent/src/RouteMap.js
@@ -33,27 +33,29 @@
 	    	this.callParent(); 
 	    },
 	    
-        items: {
-            xtype: 'gmappanel',
-            center: {
-                geoCodeAddr: '4 Yawkey Way, Boston, MA, 02215-3409, USA',
-                marker: {title: 'Fenway Park'}
-            },
-            markers: [{
-                lat: 42.339641,
-                lng: -71.094224,
-                title: 'Boston Museum of Fine Arts',
-                listeners: {
-                    click: function(e){
-                        Ext.Msg.alert('It\'s fine', 'and it\'s art.');
-                    }
-                }
-            },{
-                lat: 42.339419,
-                lng: -71.09077,
-                title: 'Northeastern University'
-            }]
-        }
+        items: [
+        	{
+	            xtype: 'gmappanel',
+	            center: {
+	                geoCodeAddr: '4 Yawkey Way, Boston, MA, 02215-3409, USA',
+	                marker: {title: 'Fenway Park'}
+	            },
+	            markers: [{
+	                lat: 42.339641,
+	                lng: -71.094224,
+	                title: 'Boston Museum of Fine Arts',
+	                listeners: {
+	                    click: function(e){
+	                        Ext.Msg.alert('It\'s fine', 'and it\'s art.');
+	                    }
+	                }
+	            },{
+	                lat: 42.339419,
+	                lng: -71.09077,
+	                title: 'Northeastern University'
+	            }]
+        	}
+        ]
 	});
 	
 //----------------------------------------------------------------------------------
